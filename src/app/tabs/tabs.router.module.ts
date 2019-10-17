e4011667e40d725e91652ecba59fd6b0491d0b36
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-10-17 16:03:01 +0200

    working on

---
+++ src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,49 @@
+import { TabsPage } from './tabs.page';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: '../home/home.module#HomePageModule'
+          }
+        ]
+      },
+      {
+        path: 'galeria',
+        children: [
+          {
+            path: '',
+            loadChildren: '../galeria/galeria.module#GaleriaPageModule'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
